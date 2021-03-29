37a573a294c0299688693b2104d3cc0508a33103
Achuth Hadnoor <armorachyuth123@gmail.com>
2021-03-29 18:34:47 +0530

    context menu options

--- main.js
+++ main.js
@@ -1,52 +1,58 @@
 'use strict'
 
-const { app, Tray, clipboard } = require("electron");
+const { app, Tray, clipboard, Menu } = require("electron");
 const { join } = require("path");
 const screenshot = require('screenshot-desktop')
-const {homedir} = require('os');
+const { homedir } = require('os');
 let _tray;
-let format = 'jpg'; 
+let format = 'jpg';
+let saveToFolder = true;
+let copyToClipBoard = true;
 
 if (app.dock) app.dock.hide();
 
 function createTray() {
-    const appIcon = join(__dirname,"static/light.png");
-    _tray = new Tray(appIcon);
-    _tray.setToolTip('SilentShot | click to capture');
-    _tray.on('click',()=>{
-      let filename = `${homedir}/Downloads/capture${Date.now()}.${format}`;
-      screenshot({filename}).then(img=>{
-        console.log('copied to clipboard');
-        clipboard.writeImage(img);
-      });
-    })
-    _tray.on('right-click',()=>{
-      // create context menu 
-      const contextMenu = [{
-        label: "copy image to clipboard / save to disk only ",
+  const appIcon = join(__dirname, "static/light.png");
+  _tray = new Tray(appIcon);
+  _tray.setToolTip('SilentShot | click to capture');
+  _tray.on('click', () => {
+    let filename = `${homedir}/Downloads/capture${Date.now()}.${format}`;
+    screenshot({ filename }).then(img => {
+      console.log('copied to clipboard');
+      clipboard.writeImage(img);
+    });
+  })
+  _tray.on('right-click', () => {
+    // create context menu
+    const contextMenu = [{
+      label: "save options",
+      submenu: [{
         accelerator: process.platform === 'darwin' ? 'Alt+Cmd+L' : 'Alt+Ctrl+L',
+        label: 'Save to folder',
+        type: "checkbox",
         click: () => {
-          clickTray();
-          tray.setTitle('')
-        }
+          saveToFolder = saveToFolder ? false : true;
+        },
+        checked: saveToFolder,
       }, {
-        label: 'Stop recording',
-        click: async () => {
-          await stopRecording();
-          return;
-        }
-      },
-      {
-        role: 'quit',
-        accelerator: process.platform === 'darwin' ? 'Alt+Shift+L' : 'Alt+Shift+L',
-      }]
-      const TrayMenu = Menu.buildFromTemplate(contextMenu);
-      _tray.popUpContextMenu(TrayMenu);
-    })
+        label: 'Copy to clipboard',
+        type: "checkbox",
+        click: () => {
+          copyToClipBoard = copyToClipBoard ? false : true;
+          console.log(copyToClipBoard);
+        },
+        checked: copyToClipBoard,
+      }],
+    },
+    {
+      role: 'quit',
+      accelerator: process.platform === 'darwin' ? 'Alt+Shift+L' : 'Alt+Shift+L',
+    }]
+    const TrayMenu = Menu.buildFromTemplate(contextMenu);
+    _tray.popUpContextMenu(TrayMenu);
+  })
 }
 
-app.on('ready',() => {
-    createTray();
+app.on('ready', () => {
+  createTray();
 })
-
-
